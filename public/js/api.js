188d6939a0a5a917da81584d25fadfec42974538
SAYAN698 <sayansadhukha@gmail.com>
2022-09-24 11:53:43 +0530

    init

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "cb830d8107db5790e9eace2d8fd6199c";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
